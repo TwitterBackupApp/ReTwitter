6342802510280bb155a617e4cb8be92d65fbf210
Sophia Kiryakova <sophia.kiryakova@gmail.com>
2018-05-04 14:26:02 +0300

    Statistics graphics added with actual data in Admin area.

--- ReTwitter.DTO/StatisticsModels/UserStatisticsModel.cs
+++ ReTwitter.DTO/StatisticsModels/UserStatisticsModel.cs
@@ -5,12 +5,19 @@ namespace ReTwitter.DTO.StatisticsModels
     public class UserStatisticsModel
     {
         public string UserName { get; set; }
+
         public string UserId { get; set; }
+
         public DateTime UserNameCreatedOn { get; set; }
+
         public string ActiveStatus { get; set; }
+
         public int ActivelyFollowedAccountsCount { get; set; }
+
         public int DeletedAccountsCount { get; set; }
+
         public int SavedTweetsCount { get; set; }
+
         public int DeletedTweetsCount { get; set; }
     }
 }
